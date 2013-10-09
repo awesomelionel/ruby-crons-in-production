a86ed731b7a8629ac3cff8f13cec6e3cf8720398
Lionel MBA13 <lionel.tan@zalora.com.my>
2013-10-09 13:08:19 +0800

    getting ruby crons to work on production crons

--- script.sh
+++ script.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
-source /usr/local/rvm/rubies/ruby-2.0.0-p0/bin/ruby
-cd /home/deployer/ruby-crons-in-production/
+#Make sure you inherit the shell in the cron
+#/bin/bash -l -c
+#source /usr/local/rvm/rubies/ruby-2.0.0-p0/bin/ruby
+#cd /home/deployer/ruby-crons-in-production/
 
-bundle exec script.rb
+#bundle exec script.rb
+
+ruby -v
+gem list
+
+ruby ~/ruby-crons-in-production/script.rb
